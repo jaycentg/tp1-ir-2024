fd0411252ea7db79715cec6c8bbf923f7de2f716
Jaycent Gunawan Ongris <jaycent.gunawan@ui.ac.id>
2023-09-15 18:38:00 +0700

    init: template code

---
+++ bsbi.py
@@ -0,0 +1,203 @@
+import os
+import pickle
+import contextlib
+import heapq
+import time
+
+from index import InvertedIndexReader, InvertedIndexWriter
+from util import IdMap, sort_intersect_list
+from compression import StandardPostings, VBEPostings
+
+""" 
+Ingat untuk install tqdm terlebih dahulu
+pip intall tqdm
+"""
+from tqdm import tqdm
+
+class BSBIIndex:
+    """
+    Attributes
+    ----------
+    term_id_map(IdMap): Untuk mapping terms ke termIDs
+    doc_id_map(IdMap): Untuk mapping relative paths dari dokumen (misal,
+                    /collection/0/gamma.txt) to docIDs
+    data_path(str): Path ke data
+    output_path(str): Path ke output index files
+    postings_encoding: Lihat di compression.py, kandidatnya adalah StandardPostings,
+                    VBEPostings, dsb.
+    index_name(str): Nama dari file yang berisi inverted index
+    """
+    def __init__(self, data_path, output_path, postings_encoding, index_name = "main_index"):
+        self.term_id_map = IdMap()
+        self.doc_id_map = IdMap()
+        self.data_path = data_path
+        self.output_path = output_path
+        self.index_name = index_name
+        self.postings_encoding = postings_encoding
+
+        # Untuk menyimpan nama-nama file dari semua intermediate inverted index
+        self.intermediate_indices = []
+
+    def save(self):
+        """Menyimpan doc_id_map and term_id_map ke output directory via pickle"""
+
+        with open(os.path.join(self.output_path, 'terms.dict'), 'wb') as f:
+            pickle.dump(self.term_id_map, f)
+        with open(os.path.join(self.output_path, 'docs.dict'), 'wb') as f:
+            pickle.dump(self.doc_id_map, f)
+
+    def load(self):
+        """Memuat doc_id_map and term_id_map dari output directory"""
+
+        with open(os.path.join(self.output_path, 'terms.dict'), 'rb') as f:
+            self.term_id_map = pickle.load(f)
+        with open(os.path.join(self.output_path, 'docs.dict'), 'rb') as f:
+            self.doc_id_map = pickle.load(f)
+
+    def start_indexing(self):
+        """
+        Base indexing code
+        BAGIAN UTAMA untuk melakukan Indexing dengan skema BSBI (blocked-sort
+        based indexing)
+
+        Method ini scan terhadap semua data di collection, memanggil parse_block
+        untuk parsing dokumen dan memanggil invert_write yang melakukan inversion
+        di setiap block dan menyimpannya ke index yang baru.
+        """
+        # loop untuk setiap sub-directory di dalam folder collection (setiap block)
+        for block_path in tqdm(sorted(next(os.walk(self.data_path))[1])):
+            td_pairs = self.parsing_block(block_path)
+            index_id = 'intermediate_index_'+block_path
+            self.intermediate_indices.append(index_id)
+            with InvertedIndexWriter(index_id, self.postings_encoding, path = self.output_path) as index:
+                self.write_to_index(td_pairs, index)
+                td_pairs = None
+    
+        self.save()
+
+        with InvertedIndexWriter(self.index_name, self.postings_encoding, path = self.output_path) as merged_index:
+            with contextlib.ExitStack() as stack:
+                indices = [stack.enter_context(InvertedIndexReader(index_id, self.postings_encoding, path=self.output_path))
+                               for index_id in self.intermediate_indices]
+                self.merge_index(indices, merged_index)
+
+    def parsing_block(self, block_path):
+        """
+        Lakukan parsing terhadap text file sehingga menjadi sequence of
+        <termID, docID> pairs.
+
+        Gunakan tools available untuk Stemming Bahasa Indonesia Seperti
+        MpStemmer: https://github.com/ariaghora/mpstemmer 
+        Jangan gunakan PySastrawi untuk stemming karena kode yang tidak efisien dan lambat.
+
+        JANGAN LUPA BUANG STOPWORDS! Kalian dapat menggunakan PySastrawi 
+        untuk menghapus stopword atau menggunakan sumber lain seperti:
+        - Satya (https://github.com/datascienceid/stopwords-bahasa-indonesia)
+        - Tala (https://github.com/masdevid/ID-Stopwords)
+
+        Untuk "sentence segmentation" dan "tokenization", bisa menggunakan
+        regex atau boleh juga menggunakan tools lain yang berbasis machine
+        learning.
+
+        Parameters
+        ----------
+        block_path : str
+            Relative Path ke directory yang mengandung text files untuk sebuah block.
+
+            CATAT bahwa satu folder di collection dianggap merepresentasikan satu block.
+            Konsep block di soal tugas ini berbeda dengan konsep block yang terkait
+            dengan operating systems.
+
+        Returns
+        -------
+        List[Tuple[Int, Int]]
+            Returns all the td_pairs extracted from the block
+            Mengembalikan semua pasangan <termID, docID> dari sebuah block (dalam hal
+            ini sebuah sub-direktori di dalam folder collection)
+
+        Harus menggunakan self.term_id_map dan self.doc_id_map untuk mendapatkan
+        termIDs dan docIDs. Dua variable ini harus persis untuk semua pemanggilan
+        parse_block(...).
+        """
+        # TODO
+        return []
+
+    def write_to_index(self, td_pairs, index):
+        """
+        Melakukan inversion td_pairs (list of <termID, docID> pairs) dan
+        menyimpan mereka ke index. Disini diterapkan konsep BSBI dimana 
+        hanya di-mantain satu dictionary besar untuk keseluruhan block.
+        Namun dalam teknik penyimpanannya digunakan srategi dari SPIMI
+        yaitu penggunaan struktur data hashtable (dalam Python bisa
+        berupa Dictionary)
+
+        ASUMSI: td_pairs CUKUP di memori
+
+        Parameters
+        ----------
+        td_pairs: List[Tuple[Int, Int]]
+            List of termID-docID pairs
+        index: InvertedIndexWriter
+            Inverted index pada disk (file) yang terkait dengan suatu "block"
+        """
+        term_dict = {}
+        for term_id, doc_id in td_pairs:
+            if term_id not in term_dict:
+                term_dict[term_id] = set()
+            term_dict[term_id].add(doc_id)
+        for term_id in sorted(term_dict.keys()):
+            index.append(term_id, sorted(list(term_dict[term_id])))
+
+    def merge_index(self, indices, merged_index):
+        """
+        Lakukan merging ke semua intermediate inverted indices menjadi
+        sebuah single index.
+
+        Ini adalah bagian yang melakukan EXTERNAL MERGE SORT
+
+        Parameters
+        ----------
+        indices: List[InvertedIndexReader]
+            A list of intermediate InvertedIndexReader objects, masing-masing
+            merepresentasikan sebuah intermediate inveted index yang iterable
+            di sebuah block.
+
+        merged_index: InvertedIndexWriter
+            Instance InvertedIndexWriter object yang merupakan hasil merging dari
+            semua intermediate InvertedIndexWriter objects.
+        """
+        # TODO
+
+    def boolean_retrieve(self, query):
+        """
+        Melakukan boolean retrieval untuk mengambil semua dokumen yang
+        mengandung semua kata pada query. Jangan lupa lakukan pre-processing
+        yang sama dengan yang dilakukan pada proses indexing!
+        (Stemming dan Stopwords Removal)
+
+        Parameters
+        ----------
+        query: str
+            Query tokens yang dipisahkan oleh spasi
+
+            contoh: Query "universitas indonesia depok" artinya adalah
+                    boolean query "universitas AND indonesia AND depok"
+
+        Result
+        ------
+        List[str]
+            Daftar dokumen terurut yang mengandung sebuah query tokens.
+            Harus mengembalikan EMPTY LIST [] jika tidak ada yang match.
+
+        JANGAN LEMPAR ERROR/EXCEPTION untuk terms yang TIDAK ADA di collection.
+        """
+        # TODO
+        return []
+
+
+if __name__ == "__main__":
+
+    BSBI_instance = BSBIIndex(data_path = 'collections', \
+                              postings_encoding = VBEPostings, \
+                              output_path = 'index')
+    BSBI_instance.start_indexing() # memulai indexing!
---
+++ util.py
@@ -0,0 +1,98 @@
+class IdMap:
+    """
+    Ingat kembali di kuliah, bahwa secara praktis, sebuah dokumen dan
+    sebuah term akan direpresentasikan sebagai sebuah integer. Oleh
+    karena itu, kita perlu maintain mapping antara string term (atau
+    dokumen) ke integer yang bersesuaian, dan sebaliknya. Kelas IdMap ini
+    akan melakukan hal tersebut.
+    """
+
+    def __init__(self):
+        """
+        Mapping dari string (term atau nama dokumen) ke id disimpan dalam
+        python's dictionary; cukup efisien. Mapping sebaliknya disimpan dalam
+        python's list.
+
+        contoh:
+            str_to_id["halo"] ---> 8
+            str_to_id["/collection/dir0/gamma.txt"] ---> 54
+
+            id_to_str[8] ---> "halo"
+            id_to_str[54] ---> "/collection/dir0/gamma.txt"
+        """
+        self.str_to_id = {}
+        self.id_to_str = []
+
+    def __len__(self):
+        """Mengembalikan banyaknya term (atau dokumen) yang disimpan di IdMap."""
+        # TODO
+        return 0
+
+    def __get_id(self, s):
+        """
+        Mengembalikan integer id i yang berkorespondensi dengan sebuah string s.
+        Jika s tidak ada pada IdMap, lalu assign sebuah integer id baru dan kembalikan
+        integer id baru tersebut.
+        """
+        # TODO
+        return 0
+    
+    def __get_str(self, i):
+        """Mengembalikan string yang terasosiasi dengan index i."""
+        # TODO
+        return ""
+
+    def __getitem__(self, key):
+        """
+        __getitem__(...) adalah special method di Python, yang mengizinkan sebuah
+        collection class (seperti IdMap ini) mempunyai mekanisme akses atau
+        modifikasi elemen dengan syntax [..] seperti pada list dan dictionary di Python.
+
+        Silakan search informasi ini di Web search engine favorit Anda. Saya mendapatkan
+        link berikut:
+
+        https://stackoverflow.com/questions/43627405/understanding-getitem-method
+
+        """
+        # TODO
+        return None
+
+def sort_intersect_list(list_A, list_B):
+    """
+    Intersects two (ascending) sorted lists and returns the sorted result
+    Melakukan Intersection dua (ascending) sorted lists dan mengembalikan hasilnya
+    yang juga terurut.
+
+    Parameters
+    ----------
+    list_A: List[Comparable]
+    list_B: List[Comparable]
+        Dua buah sorted list yang akan di-intersect.
+
+    Returns
+    -------
+    List[Comparable]
+        intersection yang sudah terurut
+    """
+    # TODO
+    return []
+
+if __name__ == '__main__':
+
+    doc = ["halo", "semua", "selamat", "pagi", "semua"]
+    term_id_map = IdMap()
+    assert [term_id_map[term] for term in doc] == [0, 1, 2, 3, 1], "term_id salah"
+    assert term_id_map[1] == "semua", "term_id salah"
+    assert term_id_map[0] == "halo", "term_id salah"
+    assert term_id_map["selamat"] == 2, "term_id salah"
+    assert term_id_map["pagi"] == 3, "term_id salah"
+
+    docs = ["/collection/0/data0.txt",
+            "/collection/0/data10.txt",
+            "/collection/1/data53.txt"]
+    doc_id_map = IdMap()
+    assert [doc_id_map[docname] for docname in docs] == [0, 1, 2], "docs_id salah"
+    
+    assert sort_intersect_list([1, 2, 3], [2, 3]) == [2, 3], "sorted_intersect salah"
+    assert sort_intersect_list([4, 5], [1, 4, 7]) == [4], "sorted_intersect salah"
+    assert sort_intersect_list([], []) == [], "sorted_intersect salah"
